ef31c987f9de4ff848df5a2379589c76c2e7fbeb
YbotMan <toby.balsley+YbotMan@gmail.com>
2023-04-09 13:08:41 -0400

    Filter No, MUI Yes, Need Splits

---
+++ src/UseEvents.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from 'react';
+
+export const useEvents = (fetchCategories) => {
+    const [events, setEvents] = useState([]);
+    const [clickedDate, setClickedDate] = useState('');
+
+    const defaultValues = {
+        secondary_category: "default",
+        tri_category: "default",
+        organizer: "TPB",
+        location: "Near Boston",
+        recurrence_rule: "",
+        owner_organizerId: "4",
+    };
+
+    useEffect(() => {
+        fetch('/api/events')
+            .then((response) => response.json())
+            .then((data) => setEvents(data));
+
+        if (fetchCategories) {
+            fetchCategories();
+        }
+    }, []);
+
+    const handleEventFormPost = async (eventData) => {
+        console.log("Post eventData:", eventData);
+        const response = await fetch("/api/events", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                ...eventData, ...defaultValues,
+                category: eventData.category,
+                secondary_category: eventData.secondary_category,
+                tri_category: eventData.tri_category,
+                organizer: eventData.organizer,
+                location: eventData.location,
+                recurrence_rule: eventData.recurrence_rule,
+                owner_organizerId: eventData.owner_organizerId,
+            }),
+        });
+
+        const createdEvent = await response.json();
+        setEvents([...events, createdEvent]);
+    };
+
+    const handleEventFormPut = async (updatedEvent) => {
+        console.log("PUT eventData:", updatedEvent);
+        const { id, ...eventData } = updatedEvent;
+
+        try {
+            const response = await fetch(`/api/events/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    ...eventData,
+                    ...defaultValues,
+                    category: eventData.primary_category,
+                    secondary_category: eventData.secondary_category,
+                    tri_category: eventData.tri_category,
+                    organizer: eventData.organizer,
+                    location: eventData.location,
+                    recurrence_rule: eventData.recurrence_rule,
+                    owner_organizerId: eventData.owner_organizerId,
+                }),
+            });
+
+            const updatedEventData = await response.json();
+            setEvents(
+                events.map((event) => (event.id === updatedEventData.id ? updatedEventData : event))
+            );
+            setClickedDate('');
+        } catch (error) {
+            console.error("Error while updating event:", error);
+        }
+    };
+
+    const handleDeleteEvent = async (eventId) => {
+        await fetch(`/api/events/${eventId}`, {
+            method: "DELETE",
+        });
+
+        setEvents(events.filter((event) => event.id !== eventId));
+    };
+
+    return {
+        events,
+        handleEventFormPost,
+        handleEventFormPut,
+        handleDeleteEvent,
+    };
+};
---
+++ x-MakePersistant.js
@@ -0,0 +1,37 @@
+// To persist the user's default region setting across sessions, you can use the browser's local storage. Local storage allows you to store key-value pairs in the user's browser, and the data is stored across browser sessions.
+// Here's an example of how you can save and load the default region from local storage in a React component:
+
+//Create a function to save the default region to local storage:
+//javascript
+
+const saveDefaultRegion = (region) => {
+    localStorage.setItem('defaultRegion', region);
+};
+
+//Create a function to load the default region from local storage:
+const loadDefaultRegion = () => {
+    return localStorage.getItem('defaultRegion') || ''; // Return an empty string if there's no saved region
+};
+
+//Use the saveDefaultRegion function in your form submission handler:
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // ...other form handling logic
+
+    // Save the selected region to local storage
+    saveDefaultRegion(selectedRegion);
+};
+
+//In your React component, use the loadDefaultRegion function to set the initial value of the region when the component mounts:
+import React, { useState, useEffect } from 'react';
+
+const MyComponent = () => {
+    const [defaultRegion, setDefaultRegion] = useState('');
+
+    useEffect(() => {
+        const savedRegion = loadDefaultRegion();
+        setDefaultRegion(savedRegion);
+    }, []);
+
+    // ...rest of the component
+};
